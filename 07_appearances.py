018be4dbec43df5be02a3304d50cffadd4e75964
pawel <pavcio1989@gmail.com>
2025-06-06 15:33:38 +0200

    NEOs appearances

---
+++ 07_appearances.py
@@ -0,0 +1,147 @@
+# Standard libraries
+import datetime
+import math
+import pathlib
+import sqlite3
+import sys
+
+# Installed libraries
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import spiceypy
+
+# Append to root directory of this repository
+sys.path.append("../")
+
+# Auxiliary module that contains the apparent magnitude
+from auxiliary import photometry
+
+
+# Accessing the NEO database
+database_dir = pathlib.Path("./databases/neos/")
+database_file = pathlib.Path("neodys.db")
+database_filepath = database_dir / database_file
+
+# Establish a connection to the database and set a cursor
+neodys_db_con = sqlite3.connect(database_filepath)
+neodys_db_cur = neodys_db_con.cursor()
+
+# Get all information from the DB. Since the DB is rather small, this won't cause any issues!
+neo_df = pd.read_sql("SELECT * FROM main", neodys_db_con)
+
+# Close the database.
+neodys_db_con.close()
+
+print(neo_df.head(5))
+print(f"No of NEOs: {neo_df.shape[0]}")
+
+# Load SPICE kernels
+spiceypy.furnsh("./kernels/spk/de432s.bsp")
+spiceypy.furnsh("./kernels/lsk/naif0012.tls")
+spiceypy.furnsh("./kernels/pck/gm_de431.tpc")
+
+# Get the G*M value of the Sun
+_, gm_sun_pre = spiceypy.bodvcd(bodyid=10, item='GM', maxn=1)
+gm_sun = gm_sun_pre[0]
+
+# Determine today's datetime
+curr_time_et = spiceypy.utc2et(datetime.datetime.now().strftime("%Y-%m-%d"))
+
+sun2earth_position_vec = spiceypy.spkgps(targ=399,
+                                     et=curr_time_et,
+                                     ref="ECLIPJ2000",
+                                     obs=10)[0]
+print(sun2earth_position_vec)
+
+# For our computations we need to convert some values from AU to km and from deg to rad
+neo_df.loc[:, "Perihel_km"] = neo_df["Perihel_AU"].apply(lambda x: spiceypy.convrt(x, "AU", "km"))
+neo_df.loc[:, "Incl_rad"] = neo_df["Incl_deg"].apply(lambda x: math.radians(x))
+neo_df.loc[:, "LongAscNode_rad"] = neo_df["LongAscNode_deg"].apply(lambda x: math.radians(x))
+neo_df.loc[:, "ArgP_rad"] = neo_df["ArgP_deg"].apply(lambda x: math.radians(x))
+neo_df.loc[:, "MeanAnom_rad"] = neo_df["MeanAnom_deg"].apply(lambda x: math.radians(x))
+neo_df.loc[:, "Epoch_JD"] = neo_df["Epoch_MJD"].apply(lambda x: x + 2400000.5)
+neo_df.loc[:, "Epoch_et"] = neo_df["Epoch_JD"].apply(lambda x: spiceypy.utc2et(str(x) + " JD"))
+
+# Only non-negative values of perihelium are accepted
+neo_df = neo_df[neo_df["Perihel_km"] > 0]
+print(neo_df.head(5))
+
+# Compute the position vector of each NEO as seen from the Sun
+neo_df.loc[:, "sun2neo_position_vec"] = \
+    neo_df.apply(lambda x: spiceypy.conics(elts=[x["Perihel_km"],
+                                                 x["Ecc_"],
+                                                 x["Incl_rad"],
+                                                 x["LongAscNode_rad"],
+                                                 x["ArgP_rad"],
+                                                 x["MeanAnom_rad"],
+                                                 x["Epoch_et"],
+                                                 gm_sun],
+                                           et=curr_time_et)[:3],
+                 axis=1)
+
+
+# Let's take the position value of (433) Eros and compare it with NASA Horizons
+print(neo_df.iloc[0]["sun2neo_position_vec"])
+
+# To compute the apparent magnitude we need to re-compute the positional vectors and convert it to
+# AU
+neo_df.loc[:, "neo2earth_position_vec"] = \
+    neo_df["sun2neo_position_vec"].apply(lambda x: sun2earth_position_vec - x)
+neo_df.loc[:, "neo2sun_position_vec"] = \
+    neo_df["sun2neo_position_vec"].apply(lambda x: -1.0 * x)
+
+neo_df.loc[:, "neo2earth_position_vec_AU"] = \
+    neo_df["neo2earth_position_vec"].apply(lambda x: [spiceypy.convrt(k, "km", "AU") for k in x])
+neo_df.loc[:, "neo2sun_position_vec_AU"] = \
+    neo_df["neo2sun_position_vec"].apply(lambda x: [spiceypy.convrt(k, "km", "AU") for k in x])
+
+# Compute the apparent magnitude of each NEO for today!
+neo_df.loc[:, "app_mag"] = \
+    neo_df.apply(lambda x: photometry.hg_app_mag(abs_mag=x["AbsMag_"],
+                                                 vec_obj2obs=x["neo2earth_position_vec_AU"],
+                                                 vec_obj2ill=x["neo2sun_position_vec_AU"],
+                                                 slope_g=x["SlopeParamG_"]), axis=1)
+
+# Let's take a look. We can use e.g., Stellarium to get a "round about" feeling whether the
+# app. mag. of Eros is correct
+print(neo_df[["Name", "app_mag"]].head(5))
+
+# Anyway ... we see that some app. mag. values appear to be outliers! The AbsMag_ does not appear
+# to be realisitc (a simple placeholder?)
+# https://newton.spacedys.com/neodys/index.php?pc=1.0
+print(neo_df[["Name", "AbsMag_", "app_mag"]].sort_values(by="app_mag"))
+
+# We set some NEOs we'd like to remove
+neos_2_del = ["2020WM"]
+neo_df = neo_df.loc[~neo_df["Name"].isin(neos_2_del)].copy()
+neo_df.reset_index(drop=True, inplace=True)
+
+# Now let's plot the distribution of the app. mag.
+# Let's set a dark background
+plt.style.use('dark_background')
+
+# Set a default font size for better readability
+plt.rcParams.update({'font.size': 14})
+
+plt.figure(figsize=(12, 6))
+counts, bins, _ = plt.hist(neo_df["app_mag"],
+                           bins=np.arange(13.0, 36.0, 1.0),
+                           color="yellow",
+                           alpha=0.7)
+
+plt.grid(linestyle="dashed", alpha=0.3)
+plt.xlim(13, 35)
+plt.xlabel("Apparent Magnitude")
+plt.ylabel("Number of NEOs")
+plt.ylim(0, 5500)
+
+# Add the limiting magnitude of Pan-STARSS as a vertical line
+# https://panstarrs.ifa.hawaii.edu/pswww/?page_id=34
+plt.vlines(24, 0, 4000, linestyles="dashed", color="lightblue")
+plt.text(24.5, 3800, "Pan-STARRS Lim. Mag.", color="lightblue")
+
+plt.savefig(f'./images/07/apparent_magnitudes_histogram.png', dpi=300)
+
+print(f"Number of NEOs Pan-STARRS could observe today: {int(sum(counts[:11]))}")
+print(f"Number of NEOs Pan-STARRS could NOT observe today: {int(sum(counts[11:]))}")
